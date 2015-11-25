2c13ed03010afb0ffece5370692fae5b7f4bbfd5
Shaun Janssens <shaunjanssens@gmail.com>
2015-11-25 12:30:44 +0100

    Simple PHP

--- src/php/index.php
+++ src/php/index.php
@@ -5,3 +5,14 @@
  * Date: 23/11/15
  * Time: 18:37
  */
+header('Content-type: application/json');
+
+if(isset($_POST["data"])) {
+    $data = $_POST["data"];
+
+    var_dump($data);
+} else {
+    header("HTTP/1.1 500 Internal Server Error");
+    die();
+}
+
--- src/php/test.php
+++ src/php/test.php
@@ -5,3 +5,10 @@
  * Date: 25/11/15
  * Time: 01:04
  */
+
+?>
+
+<form action="index.php" method="post">
+    <textarea name="data"></textarea>
+    <input type="submit" name="submit" value="Verzenden">
+</form>
